d2cf4ec6aad42bafe770b1f900b574c796c39aa9
Ali Rezaiyan <alirezaiyann@gmail.com>
2020-02-11 17:34:38 +0330

    some other item list created

---
+++ app/src/main/java/ir/alirezaiyan/profile/AvatarView.kt
@@ -0,0 +1,93 @@
+package ir.alirezaiyan.profile
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class AvatarView : AppCompatImageView {
+    private var mBitmap: Bitmap? = null
+    private var cornerRadius = 50f
+    private val mBitmapPaint = Paint()
+
+    constructor(context: Context?) : super(context) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        val a =
+            context.obtainStyledAttributes(attrs, R.styleable.AvatarView, defStyleAttr, 0)
+        cornerRadius = a
+            .getDimensionPixelSize(R.styleable.AvatarView_av_radius, cornerRadius.toInt()).toFloat()
+        a.recycle()
+        init()
+    }
+
+    private fun init() {
+        mBitmap = getBitmapFromDrawable(drawable)
+        val mBitmapShader =
+            BitmapShader(mBitmap!!, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        mBitmapPaint.shader = mBitmapShader
+        mBitmapPaint.isAntiAlias = true
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas) {
+        if (mBitmap == null) {
+            return
+        }
+        canvas.drawRoundRect(
+            RectF(0F, 0F, width.toFloat(), height.toFloat()),
+            cornerRadius,
+            cornerRadius,
+            mBitmapPaint
+        )
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+
+    fun setCornerRadius(cornerRadius: Int) {
+        this.cornerRadius = cornerRadius.toFloat()
+        invalidate()
+    }
+
+    private fun getBitmapFromDrawable(drawable: Drawable?): Bitmap? {
+        if (drawable == null) {
+            return null
+        }
+        return if (drawable is BitmapDrawable) {
+            drawable.bitmap
+        } else try {
+            val bitmap: Bitmap = if (drawable is ColorDrawable) {
+                Bitmap
+                    .createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            } else {
+                Bitmap.createBitmap(
+                    drawable.intrinsicWidth, drawable.intrinsicHeight,
+                    Bitmap.Config.ARGB_8888
+                )
+            }
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
--- app/src/main/java/ir/alirezaiyan/profile/MainActivity.kt
+++ app/src/main/java/ir/alirezaiyan/profile/MainActivity.kt
@@ -1,11 +1,12 @@
 package ir.alirezaiyan.profile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,35 +18,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         recyclerview.layoutManager = LinearLayoutManager(baseContext)
         recyclerview.adapter = Adapter()
+
+        newrelease.setOnClickListener {
+            Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("http://github.com/rezaiyan/awesomeprofile")
+            }.also { startActivity(it) }
+        }
     }
 
 
-    class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+    class Adapter() : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val layout = when (viewType) {
+                0 -> R.layout.item1
+                1 -> R.layout.item2
+                else -> R.layout.item3
+            }
+
             return ViewHolder(
                 LayoutInflater.from(parent.context).inflate(
-                    R.layout.item,
+                    layout,
                     parent,
                     false
                 )
             )
         }
 
-        override fun getItemCount() = 50
+        override fun getItemViewType(position: Int) = position
+
+        override fun getItemCount() = 3
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.onBind("Test")
         }
 
-        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            private val textVIew: TextView by lazy {
-                itemView.findViewById<TextView>(R.id.textView)
-            }
-
-            fun onBind(txt: String) {
-                textVIew.text = txt
-            }
-
-        }
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     }
 }
